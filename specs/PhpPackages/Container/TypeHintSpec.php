04a6296c14da3e830034818a7e033d8c1918e001
bound1ess <bound1ess.dev@gmail.com>
2015-02-01 17:55:55 +0600

    Write specs for TypeHint.

--- specs/PhpPackages/Container/TypeHintSpec.php
+++ specs/PhpPackages/Container/TypeHintSpec.php
@@ -3,8 +3,37 @@
 class TypeHintSpec extends \PhpSpec\ObjectBehavior
 {
 
+    public function let()
+    {
+        $this->beConstructedWith(new \ReflectionClass(new TypeHintDummy([])));
+    }
+
     public function it_is_initializable()
     {
         $this->shouldHaveType("PhpPackages\Container\TypeHint");
+    }
+
+    public function it_reads_typehints()
+    {
+        $this->read()->shouldBe([
+            [
+                "isClass"         => false,
+                "hasDefaultValue" => false,
+            ],
+            [
+                "isClass"         => true,
+                "value"           => "stdClass",
+                "hasDefaultValue" => true,
+                "defaultValue"    => null,
+            ],
+        ]);
+    }
+}
+
+class TypeHintDummy
+{
+
+    public function __construct(array $foo, \stdClass $bar = null)
+    {
     }
 }
--- src/PhpPackages/Container/TypeHint.php
+++ src/PhpPackages/Container/TypeHint.php
@@ -1,5 +1,30 @@
 <?php namespace PhpPackages\Container;
 
+use ReflectionClass;
+
 class TypeHint
 {
+
+    /**
+     * @var ReflectionClass
+     */
+    protected $reflector;
+
+    /**
+     * @param ReflectionClass $reflector
+     * @return TypeHint
+     */
+    public function __construct(ReflectionClass $reflector)
+    {
+        $this->reflector = $reflector;
+    }
+
+    /**
+     * Reads the constructor's type-hints.
+     *
+     * @return array
+     */
+    public function read()
+    {
+    }
 }
