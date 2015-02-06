9faa03cf94e49d8d9e746ffa3a5f7958d3fe25c1
bound1ess <bound1ess.dev@gmail.com>
2015-02-06 17:59:41 +0600

    Write and test (Raw).

--- specs/PhpPackages/Container/RawSpec.php
+++ specs/PhpPackages/Container/RawSpec.php
@@ -3,8 +3,18 @@
 class RawSpec extends \PhpSpec\ObjectBehavior
 {
 
+    public function let()
+    {
+        $this->beConstructedWith(123);
+    }
+
     public function it_is_initializable()
     {
         $this->shouldHaveType("PhpPackages\Container\Raw");
+    }
+
+    public function it_returns_the_stored_value()
+    {
+        $this->getValue()->shouldBe(123);
     }
 }
--- src/PhpPackages/Container/Raw.php
+++ src/PhpPackages/Container/Raw.php
@@ -2,4 +2,26 @@
 
 class Raw
 {
+
+    /**
+     * @var mixed
+     */
+    protected $value;
+
+    /**
+     * @param mixed $value
+     * @return Raw
+     */
+    public function __construct($value)
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
 }
