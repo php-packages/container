07c7c389d4e12ae0e97a2c7f305e5d46eca05e0e
bound1ess <bound1ess.dev@gmail.com>
2015-02-19 17:45:06 +0600

    Add container() helper function.

--- src/helpers.php
+++ src/helpers.php
@@ -1,8 +1,28 @@
-<?php // @codeCoverageIgnoreStart
+<?php
 
 if ( ! function_exists("raw")) {
+    /**
+     * @param mixed $value
+     * @return PhpPackages\Container\Raw
+     */
     function raw($value)
     {
         return new PhpPackages\Container\Raw($value);
+    }
+}
+
+if ( ! function_exists("container")) {
+    /**
+     * @return PhpPackages\Container\Container
+     */
+    function container()
+    {
+        static $container;
+
+        if ( ! ($container instanceof PhpPackages\Container\Container)) {
+            $container = new PhpPackages\Container\Container;
+        }
+
+        return $container;
     }
 }
